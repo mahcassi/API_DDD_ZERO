786cb785dad75ae4f56216e5cd90aee6fd53d631
Maria <mariaed.cassiano@gmail.com>
2023-04-11 22:14:52 -0300

    Add Entities and more project files

---
+++ Entities/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    internal class ApplicationUser : IdentityUser
+    {
+        public int Age { get; set; }
+        public string Cellphone { get; set; }
+        public int UserType { get; set; }
+    }
+}
---
+++ Entities/Entities/News.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    internal class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Information { get;}
+        public bool Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
