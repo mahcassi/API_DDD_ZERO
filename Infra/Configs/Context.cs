05c281068f64fac0155152d5d45b440d05cd6184
Maria <mariaed.cassiano@gmail.com>
2023-04-16 19:29:22 -0300

    Create Context

---
+++ Infra/Configs/Context.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configs
+{
+    public class Context : IdentityDbContext<ApplicationUser>
+    {
+        public Context(DbContextOptions<Context> options) : base (options) {}
+
+        public DbSet<News> News { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        public string GetStringConnection()
+        {
+            string strcon = "Server=./;Database=API_DDD_Zero;Integrated Security=True;";
+            return strcon;
+        }
+    }
+}
