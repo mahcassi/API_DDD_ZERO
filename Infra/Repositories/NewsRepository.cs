130ce02eacef5f188cb3e84feeddeb9ebbf4c8fa
Maria <mariaed.cassiano@gmail.com>
2023-04-16 20:04:04 -0300

    Created News Repository

---
+++ Infra/Repositories/NewsRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infra.Configs;
+using Infra.Repositories.Generics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositories
+{
+    internal class NewsRepository : GenericRepository<News>, INews
+    {
+        private readonly DbContextOptions<Context> _OptionsBuilder;
+        public NewsRepository()
+        {
+            _OptionsBuilder = new DbContextOptions<Context>();
+        }
+
+        public async Task<List<News>> GetAllNews(Expression<Func<News, bool>> exNews)
+        {
+            using (var data = new Context(_OptionsBuilder))
+            {
+                return await data.News.Where(exNews).AsNoTracking().ToListAsync();
+            }
+        }
+    }
+}
