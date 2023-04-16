1766949ea408c487e60c04c3edb41131fd8e8608
Maria <mariaed.cassiano@gmail.com>
2023-04-16 18:52:32 -0300

    Add interface generics

---
+++ Domain/Interfaces/Generics/IGenerics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    public interface IGenerics<T> where T : class
+    {
+
+        Task Create(T obj);
+        Task<T> Update(T obj);
+        Task<T> Delete(T obj);
+        Task<T> FindbyId(int id);
+        Task<List<T>> List();
+    }
+}
