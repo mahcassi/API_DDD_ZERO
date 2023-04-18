4db9c2ed96fbacdc9dc95b73e9537cfbc1fd1eb0
Maria <mariaed.cassiano@gmail.com>
2023-04-17 21:50:26 -0300

    Created interfaces to application project

---
+++ Application/Interfaces/Generics/IGenericsApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Generics
+{
+    public interface IGenericsApplication<T> where T : class
+    {
+        Task Create(T Obj);
+        Task Update(T Obj);
+        Task Delete(T Obj);
+        Task<T> FindbyId(int Id);
+        Task<List<T>> List();
+    }
+}
---
+++ Application/Interfaces/INewsApplication.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces.Generics;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    internal interface INewsApplication : IGenericsApplication<News>
+    {
+        Task CreateNews(News news);
+        Task UpdateNews(News news);
+        Task<List<News>> GetAllActiveNews();
+    }
+}
