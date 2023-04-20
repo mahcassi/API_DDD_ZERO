430b855bb06b581e4bc5a92de120479482523341
Maria <mariaed.cassiano@gmail.com>
2023-04-19 21:52:29 -0300

    Created UserApplication

--- Application/Applications/UserApplication.cs
+++ Application/Applications/UserApplication.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,23 @@ using System.Threading.Tasks;
 
 namespace Application.Applications
 {
-    internal class UserApplication
+    public class UserApplication : IUserApplication
     {
+        IUser _IUser;
 
+        public UserApplication(IUser IUser)
+        {
+            _IUser = IUser;
+        }
+
+        public async Task<bool> CreateUser(string email, string password, int age, string cellphone)
+        {
+            return await _IUser.CreateUser(email, password, age, cellphone);
+        }
+
+        public async Task<bool> ExistUser(string email, string password)
+        {
+            return await _IUser.ExistUser(email, password);
+        }
     }
 }
