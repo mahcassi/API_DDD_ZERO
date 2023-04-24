ffeb622260dc1534a8b3e461ba7d18b24cea44dd
Maria <mariaed.cassiano@gmail.com>
2023-04-24 19:32:34 -0300

    Configs of JWT

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,7 +8,10 @@ using Entities.Entities;
 using Infra.Configs;
 using Infra.Repositories;
 using Infra.Repositories.Generics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Token;
 
 namespace WebAPI
 {
@@ -44,6 +47,36 @@ namespace WebAPI
 
             builder.Services.AddSingleton<INewsApplication, NewsApplication>();
             builder.Services.AddSingleton<IUserApplication, UserApplication>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(option =>
+            {
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = "Teste.Securiry.Bearer",
+                    ValidAudience = "Teste.Securiry.Bearer",
+                    IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+                };
+
+                option.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = context =>
+                    {
+                        Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
             var app = builder.Build();
 
